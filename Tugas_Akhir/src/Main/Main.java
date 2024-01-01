0c2b3d194189276206c58fd986e93faef8b274ce
ToaStyToaFu <115855066+ToaStyToaFu@users.noreply.github.com>
2024-01-01 19:00:01 +0700

    Add files via upload

---
+++ Tugas_Akhir/src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int select;
+		
+		
+	}
+}
---
+++ Tugas_Akhir/src/Restaurants/MainRestaurant.java
@@ -0,0 +1,5 @@
+package Restaurants;
+
+public class MainRestaurant extends Restaurant{
+
+}
