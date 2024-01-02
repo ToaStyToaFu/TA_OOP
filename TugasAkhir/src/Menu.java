770a4d283fa448f50169bb7c89833162cf4886eb
melsonmp <152739338+melsonmp@users.noreply.github.com>
2024-01-02 17:02:23 +0700

    Update Menu.java

--- TugasAkhir/src/Menu.java
+++ TugasAkhir/src/Menu.java
@@ -1,4 +1,27 @@
 
 public class Menu {
-
+	String id, nama, type, desc, loc;
+	int harga;
+	boolean ordered;
+	public Menu(String id, String nama, String type, int harga, String desc, String loc) {
+		super();
+		this.id = id;
+		this.nama = nama;
+		this.harga = harga;
+		this.type = type;
+		this.desc = desc;
+		this.loc = loc;
+	}
+	
+	public void viewMenu() {
+		for (int i = 0; i < newMenu.size(); i++) {
+			if (type == "special") {
+				System.out.println(i+1 + ". " + menu.id + " | " + menu.nama + " | " + menu.harga + " | " + menu.desc + " |");
+			}
+			else if (type == "local") {
+				System.out.println(i+1 + ". " + menu.id + " | " + menu.nama + " | " + menu.harga + " | " + menu.loc + " | " + menu.desc + " |");
+			}		
+		}
+	}
+	
 }
